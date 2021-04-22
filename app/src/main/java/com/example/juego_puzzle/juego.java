4c71f1992be20aaccf6dbc080f193a0d9a517281
SantigoRes <santiago.restrepov@upb.edu.co>
2021-04-22 18:19:29 -0500

    Imports necesarios para desarrollo

--- app/src/main/java/com/example/juego_puzzle/juego.java
+++ app/src/main/java/com/example/juego_puzzle/juego.java
@@ -1,6 +1,22 @@
 package com.example.juego_puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class juego extends AppCompatActivity {
+
 }
