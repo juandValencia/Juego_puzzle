0e2269627008fbc87db616f5732dbab256fa23df
SantigoRes <santiago.restrepov@upb.edu.co>
2021-04-22 18:23:41 -0500

    Funcion Cargar tablero

--- app/src/main/java/com/example/juego_puzzle/juego.java
+++ app/src/main/java/com/example/juego_puzzle/juego.java
@@ -36,5 +36,47 @@ public class juego extends AppCompatActivity {
     boolean bloqueo = false;
     final Handler handler = new Handler();
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.juego);
+        //init();
+    }
+    private void cargarTablero(){
+        imb00 = findViewById(R.id.boton00);
+        imb01 = findViewById(R.id.boton01);
+        imb02 = findViewById(R.id.boton02);
+        imb03 = findViewById(R.id.boton03);
+        imb04 = findViewById(R.id.boton04);
+        imb05 = findViewById(R.id.boton05);
+        imb06 = findViewById(R.id.boton06);
+        imb07 = findViewById(R.id.boton07);
+        imb08 = findViewById(R.id.boton08);
+        imb09 = findViewById(R.id.boton09);
+        imb10 = findViewById(R.id.boton10);
+        imb11 = findViewById(R.id.boton11);
+        imb12 = findViewById(R.id.boton12);
+        imb13 = findViewById(R.id.boton13);
+        imb14 = findViewById(R.id.boton14);
+        imb15 = findViewById(R.id.boton15);
+
+        tablero[0] = imb00;
+        tablero[1] = imb01;
+        tablero[2] = imb02;
+        tablero[3] = imb03;
+        tablero[4] = imb04;
+        tablero[5] = imb05;
+        tablero[6] = imb06;
+        tablero[7] = imb07;
+        tablero[8] = imb08;
+        tablero[9] = imb09;
+        tablero[10] = imb10;
+        tablero[11] = imb11;
+        tablero[12] = imb12;
+        tablero[13] = imb13;
+        tablero[14] = imb14;
+        tablero[15] = imb15;
+    }
+
 
 }
