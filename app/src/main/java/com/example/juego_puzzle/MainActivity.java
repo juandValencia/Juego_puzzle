dc3510a8d54ca8687326805c29467cfb74ee4ddc
SantigoRes <santiago.restrepov@upb.edu.co>
2021-04-22 18:12:04 -0500

    Navegacion ventana principal1

--- app/src/main/java/com/example/juego_puzzle/MainActivity.java
+++ app/src/main/java/com/example/juego_puzzle/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.juego_puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button play, salir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        play = findViewById(R.id.botonMainJugar);
+        salir = findViewById(R.id.botonMainSalir);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("iniciando juego...");
+                iniciarJuego();
+            }
+        });
+
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void iniciarJuego(){
+        Intent i = new Intent(this, juego.class);
+        startActivity(i);
     }
 }
